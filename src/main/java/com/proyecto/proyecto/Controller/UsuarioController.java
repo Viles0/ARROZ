725910db3b199da212bf848d93956f4b788245d4
Vilez <samueldavidmolina2007@gmail.com>
2024-11-15 11:21:48 -0500

    https://github.com/Viles0/Project.git

---
+++ src/main/java/com/proyecto/proyecto/Controller/UsuarioController.java
@@ -0,0 +1,37 @@
+package com.proyecto.proyecto.Controller;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.proyecto.proyecto.Model.Usuario;
+
+public class UsuarioController {
+
+    private List<Usuario> Usuario = new ArrayList<>(
+       Arrays.asList(new Usuario(1234, "Samuel", "Molina"))
+    );
+
+    @GetMapping("/Users")
+    public List<Usuario> getUsuarios(){
+        return Usuario;
+    }
+    @GetMapping("/{nombre}")
+    public Usuario getuserid(@PathVariable String nombre){
+        for (Usuario c : Usuario){
+            return c;
+        }
+        return null;
+    }
+
+    @PostMapping("/usuario")
+    public Usuario postuser(@RequestBody Usuario usuario){
+    Usuario.add(usuario);
+    return usuario;
+    }
+}
